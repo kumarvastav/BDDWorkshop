99a8466a2d6ec35b641d7da6ff3dd9b36ce21b88
kumarvastav <srivastav@pc22985.home>
2019-01-24 21:35:11 +0100

    Modified the tests and added gitIgnore file

--- src/test/java/SerenityTest.java
+++ src/test/java/SerenityTest.java
@@ -1,14 +1,8 @@
-import Pages.FlightOptions;
-import Pages.PlanTravel;
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
-import org.junit.After;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by dharmens on 5/9/17.
@@ -17,7 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = { "src/test/resources" },
          tags = {"@Smoke"},
-         plugin = {"html:/Users/dharmens/Downloads/Projects/Serenity/target/cucumber-parallel/*"})
+         plugin = {"html:/Users/srivastav/Documents/Projects/BDDWorkshop/target/cucumber-parallel/*"})
 public class SerenityTest {
 
     @Managed(driver = "chrome")
